47618af6f3437c547911d63af0f204464c7c6e06
Krzysiek Niżnik <chrisphilipniznik@gmail.com>
2020-11-16 19:39:57 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Route, RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {AboutComponent} from './shared/about/about.component';
 
-const routes: Route = [
-  { path: '', redirectTo: '/about', pathMath: 'full' },
+const routes: Routes = [
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
-  { path: '**', redirectTo: '/about', pathMath: 'full'}
+  { path: '**', redirectTo: '/about', pathMatch: 'full'}
 ];
 
 @NgModule({
