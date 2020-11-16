8c52f0b21094f17f98533b61cb0f6f327149cc34
Krzysiek Niżnik <chrisphilipniznik@gmail.com>
2020-11-16 19:49:41 +0100

    Add buttons in components' html classes for testing Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {AboutComponent} from './shared/about/about.component';
+import {HeaderComponent} from './shared/header/header.component';
+import {FooterComponent} from './shared/footer/footer.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/about', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
   { path: '**', redirectTo: '/about', pathMatch: 'full'}
 ];
 
