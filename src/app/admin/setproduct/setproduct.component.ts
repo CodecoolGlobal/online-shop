25e86ef69f927b21a354173fe231679fb98bf083
Krzysiek Niżnik <chrisphilipniznik@gmail.com>
2020-11-27 17:49:59 +0100

    Add angular material table to setproduct html component

--- src/app/admin/setproduct/setproduct.component.ts
+++ src/app/admin/setproduct/setproduct.component.ts
@@ -8,6 +8,11 @@ import {Component, OnInit} from '@angular/core';
 export class SetproductComponent implements OnInit {
 
   toggleField: string;
+  savedChanges = false;
+  error = false;
+  errorMessage = '';
+  dataLoading = false;
+  private querySubscription;
 
   constructor() {
   }
@@ -23,6 +28,20 @@ export class SetproductComponent implements OnInit {
       filter = filter;
     }
     this.toggleField = filter;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
