a6c096c8a73d8ea3a039f1c9450d4e9b80551ef5
Krzysiek Niżnik <chrisphilipniznik@gmail.com>
2020-12-01 16:22:31 +0100

    Import angularfire library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {AboutComponent} from './shared/about/about.component';
 import {SettingsComponent} from './settings/settings.component';
 import {AdmintabComponent} from './admin/admintab/admintab.component';
 import {ProductComponent} from './user/product/product.component';
+import {LoginComponent} from './user/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/about', pathMatch: 'full' },
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'settings', component: SettingsComponent },
   { path: 'admin', component: AdmintabComponent },
   { path: 'shop', component: ProductComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '/about', pathMatch: 'full'}
 ];
 
